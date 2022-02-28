6733721d2b678e913af296c90b184dea1a5ece3a
nw2x21 <77545750+nw2x21@users.noreply.github.com>
2022-02-28 12:09:04 -0500

    Add files via upload
    completed

--- main.js
+++ main.js
@@ -3,6 +3,104 @@
 // └─────────────────┘
 
 
+function createUser(firstName, lastName){
+    let fullName = {
+        firstName: firstName,
+        lastName: lastName
+
+    }
+    return fullName;
+
+}
+
+    
+function setAge(user, age){ 
+        user.age = age
+    return user;
+
+};
+
+
+
+    let user = {    
+        firstname: "david",
+        lastname: "jones" 
+    } 
+    let userA = setAge(user, 27)
+    console.log(setAge(user, 27));
+
+
+
+
+function incrementAge(user){
+    user.age++
+    return user;
+    }
+
+    console.log(incrementAge(userA));
+
+
+
+
+
+function fixCar(car){
+    car.needsMaintenance = false
+    return car;
+}
+
+
+function addGrades(student, newGrades){
+    for(let i = 0; i< newGrades.length; i++){
+        student.grades.push(newGrades[i])
+    }
+    return student;
+}
+
+/*function Car(make, model, year, fix); {
+    let Carmake = (typeOf("string")
+    let Carmodel = (typeOf("string")
+    let Caryear = (typeOf('Number')
+        
+     console.log(make)
+
+    }*/
+
+function addTodo(todos, newTodo){
+    todos.push(newTodo)
+       return todos;
+
+}
+
+
+
+function addSong(playlist, newSong) {
+    playlist.duration += newSong.duration
+    playlist.songs.push(newSong)
+    return playlist;
+
+}
+
+
+
+function updateReportCard(reportCard, newGrade) {
+    reportCard.grades.push(newGrade)
+    reportCard.lowestGrade = Math.min(...reportCard.grades)
+    reportCard.highestGrade = Math.max(...reportCard.grades)
+    
+    let sum = 0;
+    for (let i = 0; i < reportCard.length; i++) {
+
+        sum += reportCard.grades[i];
+    }
+    reportCard.averageGrade = sum / reportCard.grades.length;
+    
+    return reportCard;
+}
+
+
+
+
+
 
 
 
